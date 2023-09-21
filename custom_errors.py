4c1dbc84d7837441a33016638f35951163fa7472
vinesagar <38588461+vinesagar@users.noreply.github.com>
2023-09-22 00:03:39 +0530

    basic skeleton without langchain

--- custom_errors.py
+++
@@ -1,5 +0,0 @@
-class NoFileUploadedError(Exception):
-    def __init__(self, code: int = 31, message: str = "No File Was Uploaded!"):
-        self.code = code
-        self.message = message
-        super().__init__(self.message)
